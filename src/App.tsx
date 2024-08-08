9e7e0d0f11b0cc82d1ddede6d908adc5ac07d169
Maximo Ariel Aranibe <darknesswong@gmail.com>
2024-08-07 22:58:42 -0300

    Repositorio sin errores y dependencias instaladas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ReactLogo from './assets/react.svg'; // Importación correcta
+import { Link } from 'react-router-dom';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <Link to="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        </Link>
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <ReactLogo />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
